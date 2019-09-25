9ce1a5a7eba342d650e9b397175001ee600bffe6
pwalters04 <pwalters@honeywell.com>
2019-09-24 22:30:51 -0400

    starter project

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/h246632/InternDev/Okta_Angular/FullStack/simplecrm/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
